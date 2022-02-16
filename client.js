01ed1cf06eabfbe3714e9e44797147b2b5e1cbd7
scee10 <schhor05@gmail.com>
2022-02-16 21:13:28 +0000

    refactored the files

--- client.js
+++ client.js
@@ -1,4 +1,31 @@
-const {testing} = require("./play");
+// establishes a connection with the game server
+const connect = function () {
+ const conn = net.createConnection({
+   host: "165.227.47.243",
+   port: 50541
+ });
 
+ conn.on('connect', function() {
+   //this is showing you 
+   console.log("Successfully connected to game server!!");
+   //this is showing on the server side
+   conn.write('Name: Ste');
+   // // setTimeout(() => {conn.write('Move: up');}, 5000);
+   // for (let i = 0; i < 100; i++) {
+   //   setTimeout(() => {
+   //     conn.write('Move: up')
+   //     conn.write('Move: left')
+   //   ;}, 500*i);
+   // }
+ })
+ // interpret incoming data as text
+ conn.setEncoding("utf8");
+ // this is data FROM the server 
+ conn.on('data', function(data) {
+   console.log("connected user says:", data);
+ })
+ return conn;
+};
 
+module.exports = connect;
 
--- play.js
+++ play.js
@@ -1,21 +1,9 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data', function(data) {
-    console.log("connected user says:", data);
-  })
-  return conn;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+// setup interface to handle user input from stdin
 
 console.log("Connecting ...");
 connect();
-
-module.exports = connect;
+setupInput();
